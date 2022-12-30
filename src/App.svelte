9b6cbd0f830f8d53d16e5cdee4b67507bac2f89c
alli-Codes <allisonevans081@gmail.com>
2022-12-30 23:41:35 +0100

    Preventing form from submitting

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
+  import { element } from "svelte/internal";
   import Icon from "./Icon.svelte";
   let show = false;
   let type = "password";
+  let inputData = {};
   function togglePassword() {
     show = !show;
     type == "password" ? (type = "text") : (type = "password");
+    console.log(inputData.email.value, inputData.password.value);
   }
 </script>
 
@@ -14,10 +17,11 @@
       <h1>Login</h1>
       <p>Login to your account</p>
     </header>
-    <form action="">
-      <input type="text" placeholder="Email" />
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <form action="" on:click|preventDefault>
+      <input type="text" placeholder="Email" bind:this={inputData.email} />
       <section>
-        <input {type} placeholder="Password" />
+        <input {type} placeholder="Password" bind:this={inputData.password} />
         <Icon {show} {togglePassword} />
       </section>
       <button>Login</button>
