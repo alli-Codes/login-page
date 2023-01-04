422242c0d7296640236ed538db0fd868dc9b0ff5
slickcodes <hartpaulisimo@gmail.com>
2023-01-04 13:38:15 +0100

    refactor the code to work as expected

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,61 @@
 <script>
-  import loginSchema from "./schema";
-  import Icon from "./Icon.svelte";
-  let show = false;
-  let type = "password";
-  let inputData = {};
-  const togglePassword = function () {
-    show = !show;
-    type == "password" ? (type = "text") : (type = "password");
-  };
-  const validate = function () {
-    let email = inputData.email.value;
-    let password = inputData.password.value;
+    import { loginSchema, errorOutput } from './schema'
+    import Icon from './Icon.svelte'
 
-    let result = loginSchema.validate({
-      email: email,
-      password: password,
-    });
+    let error
+    errorOutput.subscribe(value => (error = value))
 
-    console.log(result);
-    // console.log(email, password);
-  };
+    let show = true
+    let inputData = {}
+
+    const togglePassword = () => (show = !show)
+
+    const validate = function () {
+        let email = inputData.email.value.trim() === ''? undefined : inputData.email.value
+        let password = inputData.password.value.trim() === ''? undefined : inputData.password.value
+
+        const result = loginSchema.validate({
+            email: email,
+            password: password
+        })
+
+    }
 </script>
 
 <div class="container">
-  <div class="wrapper">
-    <header>
-      <h1>Login</h1>
-      <p>Login to your account</p>
-    </header>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <form action="" on:click|preventDefault>
-      <input
-        on:input={validate}
-        type="text"
-        placeholder="Email"
-        bind:this={inputData.email}
-      />
-      <section>
-        <input
-          on:input={validate}
-          {type}
-          placeholder="Password"
-          bind:this={inputData.password}
-        />
-        <Icon {show} {togglePassword} />
-      </section>
-      <button>Login</button>
-      <a href=".">Forgotten Password?</a>
-    </form>
-  </div>
+    <div class="wrapper">
+        <header>
+            <h1>Login</h1>
+            <p>Login to your account</p>
+        </header>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <form action="" on:click|preventDefault>
+            <input type="text" placeholder="Email" bind:this={inputData.email} />
+            <div class="message" class:disable={!error.email}>{error.email}</div>
+            <section>
+                <input type={!show ? 'password' : 'text'} placeholder="Password" bind:this={inputData.password} />
+                <span class="icon">
+                    <Icon {show} {togglePassword} />
+                </span>
+            </section>
+            <div class="message" class:disable={!error.password}>{error.password}</div>
+            <button on:click={validate}>Login</button>
+            <a href=".">Forgotten Password?</a>
+        </form>
+    </div>
 </div>
+
+<style>
+    .disable {
+        opacity: 0;
+    }
+    button:active{
+       background:  #25074c !important;
+    }
+    a{
+        padding-top: .8em;
+    }
+    .message {
+        font-size: 0.77rem;
+    }
+</style>
--- src/schema.js
+++ src/schema.js
@@ -1,18 +1,38 @@
-import jsonValidace from "json-validace";
+import  {Schema} from "json-validace";
+
+
+import { writable } from "svelte/store";
+
+
+export const errorOutput = writable({
+  email: "",
+  password: ""
+})
+
+
+function setErrorMessage(data) {
+  // output the error message to the error container 
+  errorOutput.update(value => {
+    return {
+      ...value, [data.key]: data.errorMessages[0] ?? "✔️"
+    }
+  })
+}
 
 // create a validace schema
-const loginSchema = new jsonValidace.Schema({
-  password: {
-    type: "string",
-    required: true,
-    minLength: 10,
-    maxLength: 20,
-  },
+export const loginSchema = new Schema({
   email: {
-    type: "email",
+    type: ["email", " %key% not valid"],
     required: true,
+    func: setErrorMessage
   },
+  password: {
+    type: ["string", "%key% not valid"],
+    required: [true, "%key% feild cannot be empty!"],
+    match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "not a strong %key%!"],
+    func: setErrorMessage
+  }
 });
 
-// export loginSchema
-export default loginSchema;
+
+
